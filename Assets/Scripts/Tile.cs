8247b7ab47645555106f88d3a6d228093798cd81
Brandon Richey <diamondgfx@gmail.com>
2014-08-16 00:55:32 -0400

    WIP getting stuff working

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+	private bool _isTouched;
+	public Transform tileController;
+	private TileController _tcScript;
+
+	void Start()
+	{
+		_isTouched = false;
+		_tcScript = tileController.GetComponent<TileController>();
+	}
+
+	void OnMouseDown() {
+		Debug.Log ("i have been touched");
+	}
+}
--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -9,14 +9,36 @@ public class TileController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		int randColor = 0;
+		// RED
+		Color newColor = new Color(1.0f, 0.0f, 0.0f);
+
 		tiles = new List<Transform>();
-		for (int y = 0; y < 15; y++) {
+		for (int y = 0; y < 8; y++) {
 			for (int x = 0; x < 8; x++) {
 				var newTile = Instantiate(tilePrefab) as Transform;
 				float startX = -2.35f;
-				float startY = 4.64f;
+				float startY = 2.5f;
 				float size   = 0.67f;
 				newTile.position = new Vector3(startX + (size * x), startY + (size * y), 1);
+				randColor = Random.Range (0, 4);
+				if (randColor == 1) {
+					// GREEN
+					newColor = new Color(0.0f, 1.0f, 0.0f);
+				} else if (randColor == 2) {
+					// BLUE
+					newColor = new Color(0.0f, 0.0f, 1.0f);
+				} else if (randColor == 3) {
+					// PINK
+					newColor = new Color(1.0f, 0.0f, 1.0f);
+				} else if (randColor == 4) {
+					// YELLOW
+					newColor = new Color(1.0f, 1.0f, 0.0f);
+				} else {
+					// RED
+					newColor = new Color(1.0f, 0.0f, 0.0f);
+				}
+				newTile.GetComponent<SpriteRenderer>().color = newColor;
 				tiles.Add(newTile);
 			}
 		}
