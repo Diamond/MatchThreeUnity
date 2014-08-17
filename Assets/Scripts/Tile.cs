34cc9c49a1664f6ccd3a8ce79c054ce4cd1a2363
Brandon Richey <diamondgfx@gmail.com>
2014-08-17 11:23:53 -0400

    Working!

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,15 +4,14 @@ using System.Collections;
 public class Tile : MonoBehaviour {
 	private bool _isTouched;
 	public Transform tileController;
-	private TileController _tcScript;
+	public TileController tcScript;
 
 	void Start()
 	{
 		_isTouched = false;
-		_tcScript = tileController.GetComponent<TileController>();
 	}
 
 	void OnMouseDown() {
-		Debug.Log ("i have been touched");
+		tcScript.selectedTile = this;
 	}
 }
--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -7,8 +7,16 @@ public class TileController : MonoBehaviour {
 
 	public List<Transform> tiles;
 
+	private bool _isTouching = false;
+	private Vector3 _startPosition;
+	public Tile selectedTile;
+
+	public float jitterCorrection = 0.5f;
+
 	// Use this for initialization
 	void Start () {
+		Input.simulateMouseWithTouches = true;
+
 		int randColor = 0;
 		// RED
 		Color newColor = new Color(1.0f, 0.0f, 0.0f);
@@ -39,7 +47,55 @@ public class TileController : MonoBehaviour {
 					newColor = new Color(1.0f, 0.0f, 0.0f);
 				}
 				newTile.GetComponent<SpriteRenderer>().color = newColor;
+				newTile.GetComponent<Tile>().tcScript = this;
 				tiles.Add(newTile);
+			}
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0)) {
+			if (!_isTouching) {
+				_isTouching = true;
+				_startPosition = Input.mousePosition;
+			}
+		}
+		if (_isTouching && Input.mousePosition != _startPosition) {
+			Vector3 newPosition = Input.mousePosition;
+			if (newPosition.y > _startPosition.y) {
+				// Move down
+
+				float dy = Mathf.Abs(newPosition.y - _startPosition.y);
+				if (dy > jitterCorrection) {
+					Debug.Log ("up");
+				}
+			}
+			if (newPosition.y < _startPosition.y) {
+				// Move up
+				float dy = Mathf.Abs(newPosition.y - _startPosition.y);
+				if (dy > jitterCorrection) {
+					Debug.Log ("down");
+				}
+			}
+			if (newPosition.x < _startPosition.x) {
+				// Move left
+				float dx = Mathf.Abs(newPosition.x - _startPosition.x);
+				if (dx > jitterCorrection) {
+					Debug.Log ("Left");
+				}
+			}
+			if (newPosition.x > _startPosition.x) {
+				// Move right
+				float dx = Mathf.Abs(newPosition.x - _startPosition.x);
+				if (dx > jitterCorrection) {
+					Debug.Log ("Right");
+				}
+			}
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			if (_isTouching) {
+				_isTouching = false;
 			}
 		}
 	}
